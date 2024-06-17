edb8acbf59ff0ea09e5300561ba5092b3eb1d8de
(bot) Dipsy Wong <ycwongal@connect.ust.hk>
2024-06-18 00:14:57 +0800

    make mastermind able to display subscores

--- content/utils.py
+++ content/utils.py
@@ -1,3 +1,40 @@
+import copy
+import unittest
+
+class FreezedParam():
+    def __init__(self, value):
+        self.value = value
+
+    def __enter__(self):
+        self.passed_reference = copy.deepcopy(self.value)
+        return self.passed_reference
+
+    def __exit__(self, *args):
+        assert self.passed_reference == self.value, "Should not mutate function parameter"
+
+
+def grade_test(test_class, returns=False):
+    test = unittest.main(argv=[''],defaultTest=test_class.__name__, verbosity=0, exit=False)
+    total = test.result.testsRun
+    fails = len(test.result.failures)
+    score = total - fails
+    percentage = int(float(score)/float(total) * 100)
+    result = (total, score, fails, percentage)
+    if returns:
+        return result
+    else:
+        print("==========")
+        print(f"Score of {test_class.__name__}: {score}/{total} ({percentage}%)")
+        print("==========")
+
+def grade_tests(test_classes):
+    results = [grade_test(test_class, returns=True) for test_class in test_classes]
+    print("==========")
+    for test_class, (total, score, fails, percentage) in zip(test_classes, results):
+        print(f"Score of {test_class.__name__}: {score}/{total} ({percentage}%)")
+    print("==========")
+
+
 def assert_eq(actual, expected):
     assert actual == expected, f"expect:\n{expected}\n\nactual:\n{actual}"
 
@@ -13,14 +50,14 @@ def assert_next_grid(actual, alike):
     and there exists one and only one item in alike that is None, and its corresponding item in actual grid is either 2 or 4
     """
     extra = (sum(filterna(flatten(actual))) - sum(filterna(flatten(alike))))
-    assert extra in [2, 4], "actual should contain one and only one extra 2 or 4"
+    assert extra in [2, 4], f"actual should contain one and only one extra 2 or 4, but got {extra}"
     for i in range(16):
         rowid = i // 4
         colid = i % 4
         if alike[rowid][colid] is not None:
             assert alike[rowid][colid] == actual[rowid][colid], f"at ({rowid}, {colid}), expect {alike[rowid][colid]}, actual {actual[rowid][colid]}"
         elif actual[rowid][colid] is not None:
-            assert actual[rowid][colid] == extra, "actual should contain one and only one extra 2 or 4"
+            assert actual[rowid][colid] == extra, f"extra digit should be {extra}, but got {actual[rowid][colid]}"
 
 
 def assert_next_state(actual, expected):
